01648ecb4eba008a6a9a06062881355ae6880160
Matt Calvert <xander.raymatt@gmail.com>
2021-05-02 10:34:26 +0100

    First message added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Chat from "./Chat";
 
 function App() {
 	return (
 		<div className="app">
-			<h1>Let's build a slack clone!</h1>
-
-			{/* Header */}
-			<Header />
-			<div className="app__body">
-				{/* Side Bar */}
-				<Sidebar />
-				{/* React Router -> Chat screen */}
-			</div>
+			<Router>
+				{/* Header */}
+				<Header />
+				<div className="app__body">
+					{/* Side Bar */}
+					<Sidebar />
+					{/* React Router -> Chat screen */}
+					<Switch>
+						<Route path="/room/:roomId">
+							<Chat />
+						</Route>
+						<Route path="/">
+							<Chat />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	);
 }
